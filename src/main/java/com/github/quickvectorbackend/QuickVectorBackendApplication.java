55e6c8c0f469de90877b90cd7d4e079664f99e60
Zen Huifer <huifer97@163.com>
2023-06-14 10:16:43 +0800

    Initial commit

---
+++ src/main/java/com/github/quickvectorbackend/QuickVectorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.quickvectorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickVectorBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QuickVectorBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/quickvectorbackend/QuickVectorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.quickvectorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickVectorBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
